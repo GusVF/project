fa65ea26bd966ec869ca205c586a4ee4add0155c
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-04-06 18:26:46 -0300

    feat:requisito-01

---
+++ src/migrations/20230406184124-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+     await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      display_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    },
+       { timestamps: false}
+    );
+  },
+
+  down: async (queryInterface, _Sequelize) => queryInterface.dropTable('users'),
+};
